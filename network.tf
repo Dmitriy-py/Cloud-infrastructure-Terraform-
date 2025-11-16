a890018273cad12897a059f57462b95ab5d7453d
Dmitriy-py <80920815+Dmitriy-py@users.noreply.github.com>
2025-11-16 15:01:27 +0300

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,9 +1,7 @@
-# VPC
 resource "yandex_vpc_network" "main" {
   name = "final-project-vpc"
 }
 
-# Subnet
 resource "yandex_vpc_subnet" "main" {
   name           = "final-project-subnet-${var.default_zone}"
   zone           = var.default_zone
@@ -11,19 +9,16 @@ resource "yandex_vpc_subnet" "main" {
   v4_cidr_blocks = var.vpc_cidr
 }
 
-# Security Group
 resource "yandex_vpc_security_group" "app_sg" {
   name       = "app-security-group"
   network_id = yandex_vpc_network.main.id
 
-  # Egress (All Out)
   egress {
     protocol          = "ANY"
     v4_cidr_blocks    = var.allowed_cidr
     description       = "Allow all outgoing traffic"
   }
 
-  # Ingress (SSH, HTTP, HTTPS)
   dynamic "ingress" {
     for_each = var.ingress_ports
     content {
@@ -34,7 +29,6 @@ resource "yandex_vpc_security_group" "app_sg" {
     }
   }
 
-  # Ingress (Self-traffic)
   ingress {
     protocol          = "ANY"
     predefined_target = "self_security_group"
