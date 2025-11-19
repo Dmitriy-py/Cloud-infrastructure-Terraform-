e688c1689b4b944e93a1b15f5fecd780d260d65f
Dmitriy-py <80920815+Dmitriy-py@users.noreply.github.com>
2025-11-20 02:21:25 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,6 +7,7 @@ variable "default_zone" {
 }
 
 variable "vpc_name" {
+  description = "Name for the VPC network."
   type        = string
   default     = "final-project-vpc" 
 }
@@ -35,20 +36,12 @@ variable "vm_disk_size" {
   default     = 20
 }
 
-HEAD
 variable "vm_disk_type" {         
-
-variable "vm_disk_type" {
- bcf78197abceb08d5c70ffc1fc592f3180689d28
   type        = string
   default     = "network-ssd"
 }
 
- HEAD
 variable "container_optimized_image_id" {    
-
-variable "container_optimized_image_id" {
- bcf78197abceb08d5c70ffc1fc592f3180689d28
   type        = string
   default     = "fd8b2p3ts70pbpidnm"
 }
