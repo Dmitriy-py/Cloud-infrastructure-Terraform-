82fb247eecd541686a5c76fad9fb184d8121dbc3
Dmitriy-py <80920815+Dmitriy-py@users.noreply.github.com>
2025-11-20 03:02:56 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,6 @@ output "app_server_public_ips" {
   value = yandex_compute_instance.app_vm[*].network_interface[0].nat_ip_address
 }
 
-output "database_fqdn" {
-  value       = yandex_mdb_mysql_cluster.app_db_cluster.primary_host[0].fqdn
-}
-
 output "main_subnet_id" {
   value       = module.vpc_network.subnet_id
 }
